14401a43b7fa8852ad946cfa6fd6ba53ac620707
John BEPPU <john.beppu@gmail.com>
2016-09-23 12:42:27 -0700

    Add optionalCallback(fn) function

---
+++ index.js
@@ -0,0 +1,26 @@
+/* Transform a Promise-returning function into a function that can optionally
+ * take a callback as the last parameter instead.
+ *
+ * @param   {Function}    fn          a function that returns a Promise
+ * @param   {Object}      self        (optional) `this` to be used when applying fn
+ * @return  {Function}                a function that can take callbacks as well.
+ */
+function optionalCallback(fn, self) {
+  if (!self) {
+    self = this;
+  }
+  return function() {
+    var last = arguments[arguments.length - 1];
+    if (typeof last === 'function') {
+      return fn.apply(self, arguments).then(function(){
+        last.apply(self, arguments);
+      }).catch(function(err){
+        last(err);
+      });
+    } else {
+      return fn.apply(self, arguments);
+    }
+  };
+}
+
+module.exports = optionalCallback;
