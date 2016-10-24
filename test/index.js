4c2e621de648d015a49eeb2d9b74c46e345a9d59
John BEPPU <john.beppu@gmail.com>
2016-10-24 05:44:36 -0700

    Write real tests

--- test/index.js
+++ test/index.js
@@ -1,19 +1,20 @@
 import test from 'ava';
 import oc from '../dist/index.js';
 
-test('foo', t => {
-    t.pass();
+function testFunction(a, b) {
+  return Promise.resolve(a + b);
+}
+
+test('oc should be a function', t => {
+  t.is(typeof oc, 'function');
 });
 
-test('oc', t => {
-  if (oc != null)
-    t.pass();
-  else
-    t.fail();
+test('oc should return a function when given a function', t => {
+  var tf2 = oc(testFunction);
+  t.is(typeof tf2, 'function');
 });
 
-test('bar', async t => {
-    const bar = Promise.resolve('bar');
-
-    t.is(await bar, 'bar');
+test.cb('the transformed function should take a callback', t => {
+  var tf2 = oc(testFunction);
+  tf2(1, 2, t.end);
 });
